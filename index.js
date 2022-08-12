9d11116a5f14b420a3a00368f8734679d9d05d40
eric ogbe <tradelearn13@gmail.com>
2022-08-12 18:25:13 +0200

    Update index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+if ($(window).width() < 800) {
+    $(".mid").height('55vh')
+    $(".mid").css("background-image", 'url(./300x250x5.png)');
+    $(".mid").css("background-repeat", 'no-repeat')
+    $(".mid").css("background-size", 'cover')
+
+}
